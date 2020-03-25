d2ad1a98b3462942d213bd2a0bbbc34b96c1a136
Neetish Kumar <neetish3b@gmail.com>
2020-03-25 14:47:43 +0530

    ui changes

--- d3.js
+++ d3.js
@@ -1,28 +1,32 @@
-     document.getElementById('fetchUserDataBtn').addEventListener('click', fetchUserData);
-    
-        function fetchUserData(){
-            fetch('./data.json')
-                .then(response => response.json())
-                .then(users => {
-                    console.log(users)
-                    let output = '<h2>कोरोना लाइव ट्रैकर By Neetish Kumar</h2>';
-                    output += '<div class="main-card">';
-                    users.forEach(function(user) {
-                        output += `
-                            <div class="card">
-                            <div class="color-overlay">
-                            <h1 class="heading">
-                                ${user.displayName}
-                            </h1>
-                            <h2 class="heading2">सक्रिय केसेस: ${user.totalConfirmed}</h2>
-                            <h2 class="heading2">रिकवर हुए केसेस: ${user.totalConfirmed}</h2>
-                            <h2 class="heading2">जानलेवा केसेस: ${user.totalDeaths}</h2>
-                            <p class="last-updte">${user.lastUpdated}</p>
-                            </div>
-                            </div>
+document
+  .getElementById("fetchUserDataBtn")
+  .addEventListener("click", fetchUserData);
+
+function fetchUserData() {
+  fetch("./data.json")
+    .then(response => response.json())
+    .then(users => {
+      console.log(users);
+      let output = `<h5 class="heading5">कोरोना लाइव ट्रैकर By Neetish Kumar</h5>`
+      output += '<div class="row">';
+      users.forEach(function(user) {
+        output += `
+          <div class="col-sm-4">
+                  <div class="card">
+                  <div class="color-overlay">
+                  <h1 class="heading">
+                      ${user.displayName}
+                  </h1>
+                  <h2 class="heading2">सक्रिय केसेस: ${user.totalConfirmed}</h2>
+                  <h2 class="heading2">रिकवर हुए केसेस: ${user.totalConfirmed}</h2>
+                  <h2 class="heading2">जानलेवा केसेस: ${user.totalDeaths}</h2>
+                  <p class="last-updte">${user.lastUpdated}</p>
+                  </div>
+                  </div>
+                  </div>
                         `;
-                    });
-                    output += '</div>'
-                    document.getElementById("response").innerHTML = output;
-                });
-        }
+      });
+      output += "</div>";
+      document.getElementById("response").innerHTML = output;
+    });
+}
